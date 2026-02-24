c1c699513090536d7f83377286416d2c08646c45
helpgoodforyouofficial-eng <helpgoodforyouofficial@gmail.com>
2026-02-24 22:40:52 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'taj-calc-v27'; // Version updated for fresh sync
+const cacheName = 'taj-calc-v28'; // Version updated for fresh sync
 const assets = [
   './',
   './index.html',
@@ -61,4 +61,5 @@ self.addEventListener('fetch', e => {
     })
   );
 });
+
 
