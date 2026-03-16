dd9678353bfe0ad16c2906b02c7c160ccb50e3fc
helpgoodforyouofficial-eng <helpgoodforyouofficial@gmail.com>
2026-03-16 06:18:15 +0500

    Update script.js

--- script.js
+++ script.js
@@ -512,7 +512,7 @@ document.addEventListener('DOMContentLoaded', initSplash);
 const appInfoData = {
     developer: "Wasi Developers",
     devWA: "0334-6800959",
-    version: "v2.0.34",
+    version: "v2.0.36",
     installText: "Install Taj Karyana Calculator",
     closeText: "Close"
 };
